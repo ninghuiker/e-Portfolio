1f8911dbf4713f7a3a47ce5bc676bbf96ba10eb3
Ning Hui Ker <s10179560@connect.np.edu.sg>
2019-11-07 12:38:01 +0800

    Add New Projects, Models, Views

--- projects/urls.py
+++ projects/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("projects/", include("projects.urls")),
+    path('', views.hello_world, name='project_index'),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
